2b8d029b0892fa14a861c75a7ec92f31ef545c1b
Raquel Brito <raquelbrito662@gmail.com>
2023-11-13 23:03:17 -0300

    config gems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,12 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -73,4 +79,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+
 end
